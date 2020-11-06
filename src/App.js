3aae52ba3ef29a644167616c87ca8219e862617c
ec-ayrton <ec.ayrton@gmail.com>
2020-11-06 11:26:12 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
+import React,{ useState} from 'react'
+import AC from './images/AC.png'
+import AL from './images/AL.png'
+import AM from './images/AM.png'
+import AP from './images/AP.png'
+import BA from './images/BA.png'
+import CE from './images/CE.png'
+import ES from './images/ES.png'
+import GO from './images/GO.png'
+import MA from './images/MA.png'
+import MG from './images/MG.png'
+import MS from './images/MS.png'
+import MT from './images/MT.png'
+import PA from './images/PA.png'
+import PE from './images/PE.png'
+import PI from './images/PI.png'
+import PR from './images/PR.png'
+import RJ from './images/RJ.png'
+import RN from './images/RN.png'
+import RO from './images/RO.png'
+import RR from './images/RR.png'
+import RS from './images/RS.png'
+import SC from './images/SC.png'
+import SE from './images/SE.png'
+import SP from './images/SP.png'
+import TO from './images/TO.png'
 import './App.css';
 
 function App() {
+
+  const[search, setSearch] = useState('');
+  const[countryData,setcountryData]= useState();
+
+  
+
+  const handleSubmit =(event)=>{
+    event.preventDefault();
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados//${search}`)
+    .then(Response=> Response.json())
+    .then(CountryResponse => setcountryData(CountryResponse));
+  }
+
+  console.log(countryData)
+  console.log(search)
+  const handleChange = (event) =>{
+    
+    setSearch(event.target.value)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1 > Digite a UF a ser buscada: (ex: SP) </h1>
+      <form onSubmit={handleSubmit}>
+        <div >
+          <label>Busca por Estado </label>
+          <div >
+            <input 
+              type="text" 
+              required
+              value={search}
+              onChange={handleChange}
+              />
+            <span>
+              <button type="submit" >
+                Buscar
+              </button>
+            </span>
+          </div>
+        </div>
+      </form>
+      <div>  
+        {!countryData  &&(
+          <div>
+            <h2> Nome Estado: </h2>
+            <h2> Nome  da Regiao: </h2>
+          </div>
+        )}
+        {(countryData && !countryData.nome) &&( 
+          <h9>Dados não encontrados: insira novamente</h9>
+        )}
+        {(countryData && countryData.nome )&& (
+          <div>
+            <h2> Nome  Estado : {countryData.nome}</h2>
+            <h2>Nome da Regiao: {countryData.regiao.nome} </h2>
+            <div>
+              <img src={CE} alt="ceara" height="200px"/>
+            </div>
+          </div>  
+        )}
+      </div>
+        
+    </div>  
   );
 }
 
---
+++ src/images/images_data.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import AC from './AC'
+import AL from './AL'
+import AM from './AM'
+import AP from './AP'
+import BA from './BA'
+import CE from './CE'
+import ES from './ES'
+import GO from './GO'
+import MA from './MA'
+import MG from './MG'
+import MS from './MS'
+import MT from './MT'
+import PA from './PA'
+import PE from './PE'
+import PI from './PI'
+import PR from './PR'
+import RJ from './RJ'
+import RN from './RN'
+import RO from './RO'
+import RR from './RR'
+import RS from './RS'
+import SC from './SC'
+import SE from './SE'
+import SP from './SP'
+import TO from './TO'
+
+class CountryImgData extends Component{
+    render(){
+        return (
+
+        )
+    }
+}
+
+export default CountryImgData
